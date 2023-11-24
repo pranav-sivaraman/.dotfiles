6eed41cddbecb663ae621b4cea29fae3218605ee
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-11-23 19:21:39 -0500

    feat: neovim config

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+local keymap = vim.keymap
+
+-- Better Page Up and Down
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-b>", "<C-b>zz")
+keymap.set("n", "<C-f>", "<C-f>zz")
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,37 @@
+local opt = vim.opt
+
+-- Set Line Number
+opt.number = true
+opt.relativenumber = true
+
+-- Set Tab Rules
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- Popup Options
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+
+-- Set Colorscheme Options
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- Search Settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Cursor Line
+opt.cursorline = true
+
+-- Use System Clipboard
+opt.clipboard:append("unnamedplus")
+
+-- Set Scroll and Line Break Options
+opt.scrolloff = 999
+opt.wrap = false
+
+-- Set Color Column
+opt.colorcolumn = "80"
