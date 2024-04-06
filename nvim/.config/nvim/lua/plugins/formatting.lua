eb7f91330a42a16bf7a8d721349ebb9ce4a9daa5
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-04-06 18:15:49 -0400

    feat: add formatting

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,18 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				async = false,
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+	end,
+}
