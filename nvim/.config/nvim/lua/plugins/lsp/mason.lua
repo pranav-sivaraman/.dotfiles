99db2273d1c426292d49de9f1e2c991b25747a61
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-04-06 18:16:19 -0400

    feat: update mason

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,27 +1,27 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_tool_installer = require("mason-tool-installer")
+		mason.setup()
 
-        mason.setup()
-
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "clangd",
-                "pyright",
-                "lua_ls",
-                "bashls",
-                "cmake",
-                "yamlls",
-                "texlab",
-                "rust_analyzer",
-            },
-
-            automatic_installation = true
-        })
-    end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"clangd",
+				"black",
+				"prettier",
+				"stylua",
+				"lua_ls",
+				"bashls",
+				"cmake",
+				"yamlls",
+				"texlab",
+				"rust_analyzer",
+			},
+		})
+	end,
 }
