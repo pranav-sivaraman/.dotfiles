60ffaea2b3c60637ee941d60d28ceb8b79d0d3b6
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-04-06 18:16:06 -0400

    feat: update treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,33 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        { "nvim-treesitter/nvim-treesitter-context", opts = { enable = true}, },
-    },
-    cmd = "TSUpdateSync",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  cmd = "TSUpdateSync",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {
-                "c",
-                "cpp",
-                "cuda",
-                "lua",
-                "fortran",
-                "cmake",
-                "make",
-                "bash",
-                "python",
-                "rust",
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "cpp",
+        "cuda",
+        "lua",
+        "fortran",
+        "cmake",
+        "make",
+        "bash",
+        "python",
+        "rust",
+        "regex",
+        "markdown",
+        "markdown_inline"
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
