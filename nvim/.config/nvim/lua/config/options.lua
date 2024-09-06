56220d413cd07beac8f5d09572aed71fc541c707
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-05 22:48:51 -0400

    nvim: add options

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,31 @@
+local opt = vim.opt
+
+-- Line number
+opt.number = true
+opt.relativenumber = true
+
+-- Status information
+opt.showmode = false
+opt.showcmd = false
+
+-- Tab rules
+opt.expandtab = true
+
+-- Colorscheme
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- Scroll and line break
+opt.scrolloff = 999
+opt.wrap = true
+opt.linebreak = true
+
+-- Persistent undo
+opt.undofile = true
+
+-- Color column
+opt.colorcolumn = "80"
+
+-- No swap file
+opt.swapfile = false
