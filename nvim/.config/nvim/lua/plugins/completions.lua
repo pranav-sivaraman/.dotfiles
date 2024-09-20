09941f8dfd2c9247e7f0d23b14e44e7ce003f9e0
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-19 21:46:34 -0400

    nvim: enable tab completions

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -10,6 +10,14 @@ return {
 	},
 	opts = function()
 		local lspkind = require("lspkind")
+		local cmp = require("cmp")
+
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		return {
 			enabled = function()
 				-- disable completion in comments
@@ -33,6 +41,24 @@ return {
 					ellipsis_char = "...",
 				}),
 			},
+			mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						if #cmp.get_entries() == 1 then
+							cmp.confirm({ select = true })
+						else
+							cmp.select_next_item()
+						end
+					elseif has_words_before() then
+						cmp.complete()
+						if #cmp.get_entries() == 1 then
+							cmp.confirm({ select = true })
+						end
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
 		}
 	end,
 }
