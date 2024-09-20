60c6ad9590c94acd72eae8d75eaf8cc669dd0573
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-19 21:26:21 -0400

    nvim: add fzf keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,7 @@ keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("n", "<C-b>", "<C-b>zz")
 keymap.set("n", "<C-f>", "<C-f>zz")
 
--- -- Fzf
--- keymap.set("n", "<leader><C-t>", "<cmd>Files<CR>")
--- keymap.set("n", "<leader><C-r>", "<cmd>History<CR>")
+-- Fzf
+keymap.set("n", "<leader><C-t>", "<cmd>Files<CR>")
+keymap.set("n", "<leader><C-r>", "<cmd>History<CR>")
+keymap.set("n", "<leader><C-w>", "<cmd>Rg<CR>")
