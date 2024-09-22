3dc84de7bbf2c5054276dbd478ff58c131b37953
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-22 18:03:12 -0400

    nvim: use fzf-lua instead

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,4 +1,41 @@
 return {
-	"junegunn/fzf",
-	"junegunn/fzf.vim",
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = function()
+		local fzf = require("fzf-lua")
+		return {
+			{
+				"<leader>ff",
+				function()
+					fzf.files()
+				end,
+				desc = "Find files",
+			},
+			{
+				"<leader>fo",
+				function()
+					fzf.oldfiles()
+				end,
+			},
+			{
+				"<leader>fw",
+				function()
+					fzf.grep_project()
+				end,
+			},
+			{
+				"<leader>fb",
+				function()
+					fzf.buffers()
+				end,
+			},
+		}
+	end,
+	opts = {
+		keymap = {
+			builtin = {
+				["jk"] = "hide",
+			},
+		},
+	},
 }
