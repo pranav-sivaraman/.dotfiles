f5d626e3c8eefcfb7a6bcf930ef047d5804a05c5
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-24 16:11:02 -0400

    nvim: tmp commit till I figure out how to readd mason

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,30 +1,26 @@
 return {
 	"neovim/nvim-lspconfig",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-	},
 	config = function()
 		local lspconfig = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-
-			["lua_ls"] = function()
-				lspconfig.lua_ls.setup({
-					settings = {
-						Lua = {
-							diagnostics = { globals = { "vim" } },
-							hint = { enable = true },
-						},
-					},
-				})
-			end,
+		-- require("mason-lspconfig").setup_handlers({
+		-- 	function(server_name)
+		-- 		lspconfig[server_name].setup({
+		-- 			capabilities = capabilities,
+		-- 		})
+		-- 	end,
+		--
+		-- 	["lua_ls"] = function()
+		-- 		lspconfig.lua_ls.setup({
+		-- 			settings = {
+		-- 				Lua = {
+		-- 					diagnostics = { globals = { "vim" } },
+		-- 					hint = { enable = true },
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
 		})
 	end,
 }
