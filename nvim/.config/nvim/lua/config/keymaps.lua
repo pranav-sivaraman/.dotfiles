1f16f305a9a8c31c48e4aceb61f849f8a83ee9da
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-24 15:29:01 -0400

    nvim: remove fzf keymaps from keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,8 +5,3 @@ keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("n", "<C-b>", "<C-b>zz")
 keymap.set("n", "<C-f>", "<C-f>zz")
-
--- Fzf
-keymap.set("n", "<leader><C-t>", "<cmd>Files<CR>")
-keymap.set("n", "<leader><C-r>", "<cmd>History<CR>")
-keymap.set("n", "<leader><C-w>", "<cmd>Rg<CR>")
