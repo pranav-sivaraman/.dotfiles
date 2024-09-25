43f4acf079442416822dd127266d707e7a81b1b2
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-24 20:44:30 -0400

    zsh: p10k prompt segment changes

--- zshrc/.p10k.zsh
+++ zshrc/.p10k.zsh
@@ -52,15 +52,15 @@
     dir                       # current directory
     vcs                       # git status
     # command_execution_time  # previous command duration
-    # virtualenv              # python virtual environment
+    virtualenv              # python virtual environment
     prompt_char               # prompt symbol
   )
 
   # Right prompt segments.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     command_execution_time    # previous command duration
-    virtualenv                # python virtual environment
-    context                   # user@host
+    # virtualenv                # python virtual environment
+    # context                   # user@host
     time                      # current time
   )
 
