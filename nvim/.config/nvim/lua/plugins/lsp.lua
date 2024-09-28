5f0c0b0cdecf9d17df86de3a96fe32955eb92a22
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-28 11:52:57 -0400

    nvim: add neocmakelsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		local lspconfig = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local servers = { "clangd", "ruff", "lua_ls" }
+		local servers = { "clangd", "ruff", "lua_ls", "neocmake" }
 
 		for _, server in ipairs(servers) do
 			lspconfig[server].setup({
