7b8b3c94eae79531dbcdf4ec785ee8415e158faf
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-02-11 11:42:53 -0500

    nvim: fix yank and paste with OSC52

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,24 @@
 local opt = vim.opt
 
 opt.scrolloff = 999 -- Always center
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
