4bef189607d59144ea3bdad4df8da94bc7b56d96
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-02-11 21:40:37 -0500

    nvim: properly disable plugins and upate lazyvim.json

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
-  { "mason-lspconfig.nvim", enabled = false },
-  { "mason.nvim", enabled = false },
-  { "neo-tree.nvim", enabled = false },
-  { "catppuccin", enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
 }
