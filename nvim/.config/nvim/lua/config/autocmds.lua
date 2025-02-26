b1b162539e240fe3b642a782fb4fa98ac1939b6e
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-02-26 16:50:52 -0500

    nvim: add some ui features

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -4,3 +4,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
 		vim.opt.laststatus = 3
 	end,
 })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
