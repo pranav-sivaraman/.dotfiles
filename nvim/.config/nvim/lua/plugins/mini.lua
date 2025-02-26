f40fb1d66175b9dd126a59ec7bccb8d430caed08
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-02-26 14:34:30 -0500

    nvim: add mini.nvim

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+	"echasnovski/mini.nvim",
+	version = false,
+	config = function()
+		require("mini.ai").setup()
+		require("mini.operators").setup()
+		require("mini.surround").setup()
+	end,
+}
