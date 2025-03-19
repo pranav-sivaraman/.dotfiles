8c2e339cc50a40c12880374e7ab9e83b216f1062
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-03-19 13:00:16 -0500

    nvim: enable OSC 52 copy

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -42,3 +42,15 @@ vim.diagnostic.config({
 	},
 	underline = false, -- Disable undercurl
 })
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
