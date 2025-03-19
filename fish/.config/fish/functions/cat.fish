12036c2286329dd053100040b4f27762491b1a3a
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-03-19 13:00:16 -0500

    bat: add style=plain

--- fish/.config/fish/functions/cat.fish
+++ fish/.config/fish/functions/cat.fish
@@ -1,6 +1,6 @@
 function cat
     if command -q bat
-        command bat --theme=rose-pine --color=always $argv
+        command bat --plain --theme=rose-pine --color=always $argv
     else
         command cat $argv
     end
