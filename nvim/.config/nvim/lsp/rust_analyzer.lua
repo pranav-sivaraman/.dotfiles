93ccae80a2a31d333edfeb5d344272ae5bbb4adf
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-06-18 20:47:24 -0400

    nvim: rust_analyzer use clippy

--- nvim/.config/nvim/lsp/rust_analyzer.lua
+++ nvim/.config/nvim/lsp/rust_analyzer.lua
@@ -82,6 +82,13 @@ return {
       serverStatusNotification = true,
     },
   },
+  settings = {
+    ["rust-analyzer"] = {
+      checkOnSave = {
+        command = "clippy"
+      }
+    }
+  },
   before_init = function(init_params, config)
     -- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
     if config.settings and config.settings["rust-analyzer"] then
