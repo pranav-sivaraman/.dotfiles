ab383899de5b22a945b77606ee3cddab72abea07
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-06-18 21:40:12 -0400

    nvim: add mini-surround

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,23 @@
 return {
-  "echasnovski/mini.nvim",
-  config = function()
-    require("mini.ai").setup()
-  end,
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.ai").setup()
+      -- Note: mini.surround is configured separately below
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
 }
