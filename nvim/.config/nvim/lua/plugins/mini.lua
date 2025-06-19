da3e1776fdadf4eda36dbb51e1d87aab9036b7c1
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-06-19 08:24:34 -0400

    nvim: remove better-escape in favor of mini.nvim

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,14 +1,15 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    config = function()
-      require("mini.ai").setup()
-      -- Note: mini.surround is configured separately below
-    end,
-  },
-  {
-    "echasnovski/mini.surround",
-    opts = {
+  "echasnovski/mini.nvim",
+  config = function()
+    require("mini.ai").setup()
+
+    require("mini.keymap").setup()
+    local map_combo = require("mini.keymap").map_combo
+    local mode = { "i", "c", "x", "s" }
+    map_combo(mode, "jk", "<BS><BS><Esc>")
+    map_combo("t", "jk", "<BS><BS><C-\\><C-n>")
+
+    require("mini.surround").setup({
       mappings = {
         add = "gsa",
         delete = "gsd",
@@ -17,7 +18,7 @@ return {
         highlight = "gsh",
         replace = "gsr",
         update_n_lines = "gsn",
-      },
-    },
-  },
+      }
+    })
+  end,
 }
