a7be0e601b012cb42e470272bd1332eb8fab3e3a
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-06-23 10:14:21 -0400

    fish: add hostname to prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,6 +1,16 @@
 function fish_prompt
-    set_color $fish_color_cwd
-    echo -n (path basename $PWD)
-    set_color normal
+    set prompt_dir (path basename $PWD)
+
+    if set -q SSH_CONNECTION
+        set prompt_host (hostname)
+        set_color yellow
+        echo -n "$prompt_host:$prompt_dir"
+        set_color normal
+    else
+        set_color $fish_color_cwd
+        echo -n $prompt_dir
+        set_color normal
+    end
+
     echo -n ' ) '
 end
