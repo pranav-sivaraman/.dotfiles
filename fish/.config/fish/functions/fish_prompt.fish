dae705c6566571e6d54281ead1974d09cfadf32a
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-06-23 10:32:59 -0400

    fish: update fish prompt color

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,16 +1,15 @@
 function fish_prompt
-    set prompt_dir (path basename $PWD)
-
     if set -q SSH_CONNECTION
+        set_color $fish_color_host
+        set prompt_user (id -un)
         set prompt_host (hostname)
-        set_color yellow
-        echo -n "$prompt_host:$prompt_dir"
-        set_color normal
-    else
-        set_color $fish_color_cwd
-        echo -n $prompt_dir
-        set_color normal
+        echo -n "$prompt_user@$prompt_host:"
     end
 
+    set_color $fish_color_cwd
+    set prompt_dir (path basename $PWD)
+    echo -n $prompt_dir
+
+    set_color normal
     echo -n ' ) '
 end
