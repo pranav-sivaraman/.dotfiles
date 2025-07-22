9b45852b5df39577f4b638e66f3805929dcfe86a
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-07-22 10:06:57 -0400

    nvim: better window navigation

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
